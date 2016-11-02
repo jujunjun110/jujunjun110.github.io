74ad9008f8e37644c6c2ae27f43f2d37ca2a59bb
Jun Ito <jun_ito@voyagegroup.com>
2016-11-02 11:59:41 +0900

    Update ベクトルの操作を非破壊的にするためにcopy()をかませる

--- crawlingcursor/js/app.js
+++ crawlingcursor/js/app.js
@@ -14,13 +14,13 @@ AFRAME.registerComponent('my-ray', {
             var intersection = getNearestIntersection(e.detail.intersections);
             if (!intersection) {return;}
 
+            // cursorの座標 = 交差地点の座標 + 法線ベクトル×0.1 （少しだけ交差地点から浮かせる）
+            var cursorPosition = new THREE.Vector3().addVectors(intersection.point, intersection.face.normal.copy().multiplyScalar(0.1));
+            data.target.setAttribute("position", cursorPosition);
+
             // look atの方向の絶対座標 = 交差地点の座標 + 法線ベクトル
             var lookAtTarget = new THREE.Vector3().addVectors(intersection.point, intersection.face.normal);
             data.target.setAttribute("look-at", lookAtTarget);
-
-            // cursorの座標 = 交差地点の座標 + 法線ベクトル×0.1 （少しだけ交差地点から浮かせる）
-            var cursorPosition = new THREE.Vector3().addVectors(intersection.point, intersection.face.normal.multiplyScalar(0.1));
-            data.target.setAttribute("position", cursorPosition);
 
             // 最も近い交差地点を取得する
             function getNearestIntersection(intersections) {
