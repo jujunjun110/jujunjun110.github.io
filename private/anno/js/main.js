f1646d241c430d79abfa3cb9c480a18d3e114008
Jun Ito <jun_ito@voyagegroup.com>
2016-11-17 18:03:33 +0900

    Update やっぱり上げられなかったので中止

--- private/anno/js/main.js
+++
@@ -1,6 +0,0 @@
-var aframe = require("aframe");
-// var html2canvas = require("html2canvas");
-// var htmlshader = require("aframe-html-shader");
-// var lookat = require("aframe-look-at-component");
-// var nunjucks = require("nunjucks");
-// var kframe = require("k-frame");
